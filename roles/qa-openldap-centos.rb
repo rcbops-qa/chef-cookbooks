b3772be01b8db20ff0dae8b840977aec94719425
john5223 <john.curran@rackspace.com>
2013-06-17 11:28:34 -0500

    Changed ldap environments

--- roles/qa-openldap-centos.rb
+++ roles/qa-openldap-centos.rb
@@ -5,13 +5,13 @@ run_list(
   "recipe[yum::yum]",
   "recipe[yum::epel]",
   "recipe[openssh]",
-  "recipe[openldap::server]"         
+  "recipe[openldap::server]"
 )
 default_attributes(  { "openldap" => {
-                            "rootpw" => "{SSHA}rcZMditrBFedx2lGWSVjMUnWLLz+kEjV", 
-                            "basedn" => "dc=dev,dc=rcbops,dc=me",
+                            "rootpw" => "{SSHA}8XIbA1eWvWxXdS1oCuPn4T9HUx9J9hvX", 
+                            "basedn" => "dc=rcb,dc=me",
                             "server" => "ldap.rcbops",
-                            "auth_bindpw" => "{SSHA}rcZMditrBFedx2lGWSVjMUnWLLz+kEjV",
+                            "auth_bindpw" => "{SSHA}8XIbA1eWvWxXdS1oCuPn4T9HUx9J9hvX",
                             "slapd_type" => "master"
                             }   
                       }
--- roles/qa-openldap.rb
+++ roles/qa-openldap.rb
@@ -5,10 +5,10 @@ run_list(
   "recipe[openldap::server]"
 )
 default_attributes(  { "openldap" => {
-                            "rootpw" => "{SSHA}rcZMditrBFedx2lGWSVjMUnWLLz+kEjV", 
-                            "basedn" => "dc=dev,dc=rcbops,dc=me",
+                            "rootpw" => "{SSHA}8XIbA1eWvWxXdS1oCuPn4T9HUx9J9hvX", 
+                            "basedn" => "dc=rcb,dc=me",
                             "server" => "ldap.rcbops",
-                            "auth_bindpw" => "{SSHA}rcZMditrBFedx2lGWSVjMUnWLLz+kEjV",
+                            "auth_bindpw" => "{SSHA}8XIbA1eWvWxXdS1oCuPn4T9HUx9J9hvX",
                             "slapd_type" => "master"
                             }   
                       }
